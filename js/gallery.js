2fc916ebaeb216a13fc4e995334fd8cfeee4781c
Mikitenko <farmer@ua.fm>
2014-07-14 14:51:27 +0300

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -251,8 +251,8 @@ window.onload = function(){
        animatePropOpacity({ // добавляем Анимацию на появление нашей картинки
         elem: this.bigPicture,
         prop: 'opacity',
-        start: 1,
-        end: 0,
+        start: 0,
+        end: 1,
         step: 0.1,
         duration: 300,
         delay: 10
