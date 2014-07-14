a1d0bf9100c014b8f71cd6128f78e03e9076fbd2
Mikitenko <farmer@ua.fm>
2014-07-14 15:28:19 +0300

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -247,9 +247,7 @@ window.onload = function(){
     this.removeClassItemList(); // удаляем класс у всех элементов списка
     addClass(nextItem, 'current'); // добавляем класс к элементу списка
     this.bigPicture.src = nextItem.childNodes[0].href; // добавляем изображение в большей блок
-    this.bigPicture.style.display = 'none';
     if(this.bigPicture.complete) {
-      this.bigPicture.style.display = 'block'; 
       this.bigPicture.style['opacity'] = '0'; 
       animatePropOpacity({ // добавляем Анимацию на появление нашей картинки
         elem: this.bigPicture,
