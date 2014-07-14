62cf46a0a8367e3fcdb23da9c3560118fae23a05
Mikitenko <farmer@ua.fm>
2014-07-14 14:50:19 +0300

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -246,17 +246,19 @@ window.onload = function(){
     }
     this.removeClassItemList(); // удаляем класс у всех элементов списка
     addClass(nextItem, 'current'); // добавляем класс к элементу списка
-    animatePropOpacity({ // добавляем Анимацию на появление нашей картинки
-      elem: this.bigPicture,
-      prop: 'opacity',
-      start: 1,
-      end: 0,
-      step: 0.1,
-      duration: 300,
-      delay: 10
-    });
-    this.bigPicture.style.opacity = '0'; //прячем старую картинку, пока не загрузилась новая
     this.bigPicture.src = nextItem.childNodes[0].href; // добавляем изображение в большей блок
+    this.bigPicture.onload = function () {
+       animatePropOpacity({ // добавляем Анимацию на появление нашей картинки
+        elem: this.bigPicture,
+        prop: 'opacity',
+        start: 1,
+        end: 0,
+        step: 0.1,
+        duration: 300,
+        delay: 10
+      });	
+    };
+   
   };
 
 
