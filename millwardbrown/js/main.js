83f81a20006a31f073510829033967e1ae741961
Roman <roman.mikitenko@ilogos-ua.com>
2014-07-31 13:46:34 +0300

    Add coin animation

--- millwardbrown/js/main.js
+++ millwardbrown/js/main.js
@@ -9,23 +9,67 @@ $(function() {
     return images;
   }
 
-  function handleDropEvent( event, ui ) {
+  function coinAnimate(obj){
+    var childObj = obj.children('div.shelf');
+    coin = $('<div class="coin">')
+      .insertAfter(bottomSide)
+      .css({
+        'width': money.width(),
+        'height': money.height(),
+        'left': money.offset().left,
+        'top': money.offset().top
+      })
+      .animate({
+        'width': shelf.width(),
+        'height': shelf.height(),
+        "top": childObj.offset().top,
+        "left": childObj.offset().left
+      }, 250, function() {
+        coin.remove();
+        childObj.addClass('shelf_gold');
+      });
+  }
+
+  function setMaxExeuctionInterval( callback, delay, maxExecutions ){
+    var intervalCallback = function(){
+      var self = intervalCallback;
+      if ( 'undefined' == typeof self.executedIntervals ){
+        self.executedIntervals = 1;
+      }
+      if ( self.executedIntervals == maxExecutions ){
+        clearInterval( self.interval )
+      }
+      self.executedIntervals += 1;
+      callback();
+    };
+    intervalCallback.interval = setInterval( intervalCallback, delay );
+  }
+
+  function handleDropEvent(event, ui) {
     var draggable = ui.draggable;
-    $(this).children('div.shelf').addClass('shelf_gold');
+    var thisScreen = $(this);
     draggable.addClass('display_none');
+    setMaxExeuctionInterval( function(){
+      coinAnimate(thisScreen);
+    }, 300, 5);
+
   }
 
-  function handleMoveEvent( event, ui ) {
+  function handleMoveEvent(event, ui) {
 
   }
 
+  var coin;
   var gameContainer = $('.game_container');
   var moneyMove = $('.money_move');
+  var money = $('.money');
+  var shelf = $('.shelf');
+  var bottomSide = $('.bottom_side');
   var imageSceenWrapp = $('.image_sceen_wrapp');
 
   gameContainer.addClass('display_none');
 
-  var imageLinks = ['img/main_bg.png','img/screen.png','img/flower2.png','img/flower1.png','img/player.png','img/money.png','img/small_money.png'];
+  var imageLinks = ['img/main_bg.png','img/screen.png','img/flower2.png','img/flower1.png','img/player.png','img/money.png','img/small_money.png','img/coin_anim.png'];
 
   if ((preload(imageLinks)).length === imageLinks.length){
     setInterval(function() {
@@ -44,6 +88,8 @@ $(function() {
   imageSceenWrapp.droppable( {
     drop: handleDropEvent
   });
+
+
 
 
 
