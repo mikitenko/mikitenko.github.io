cd84b22b75a4962680a7c32a593606bf5580d512
Mikitenko <roman.mikiteko@maxymiser.com>
2016-09-15 18:11:03 +0300

    3d

--- samsung-banner/js/main.js
+++ samsung-banner/js/main.js
@@ -3,19 +3,19 @@ var adB = {
         return document.querySelector(selector);
     },
 
-    hasClass: function (selector, className) {
-        return this.node(selector).className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+    hasClass: function (node, className) {
+        return node.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
     },
 
 
-    addClass: function (selector, className) {
-        if (!this.hasClass(selector, className)) this.node(selector).className += ' ' + className;
+    addClass: function (node, className) {
+        if (!this.hasClass(node, className)) node.className += ' ' + className;
     },
 
-    removeClass: function (selector, className) {
-        if (this.hasClass(selector, className)) {
+    removeClass: function (node, className) {
+        if (this.hasClass(node, className)) {
             var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-            this.node(selector).className = this.node(selector).className.replace(reg, '');
+            node.className = node.className.replace(reg, '');
         }
     },
 
@@ -36,20 +36,76 @@ var adB = {
         if (document && document.readyState === 'complete') return handler();
         if (window.addEventListener) window.addEventListener('DOMContentLoaded', handler, false);
         else if (window.attachEvent) window.attachEvent('onload', handler);
+    },
+
+    fadeDescription: function (node) {
+        this.addClass(node, 'fade');
+        setTimeout(function () {
+            adB.removeClass(node, 'fade');
+        }, 200);
     }
+
 };
 
 
-var images = [
-    '/samsung-banner/images/phone_sprite.png',
-    '/samsung-banner/images/description.png',
-    '/samsung-banner/images/dragtext.png',
-    '/samsung-banner/images/cta.png'
-];
+adB.domReady(function () {
 
 
-adB.imageOnload(images, function (data) {
-    adB.domReady(function () {
-        adB.addClass('.main-container', 'show');
+    var images = [
+        '/samsung-banner/images/phone_sprite.png',
+        '/samsung-banner/images/description.png',
+        '/samsung-banner/images/dragtext.png',
+        '/samsung-banner/images/cta.png'
+    ];
+
+    var mainContainer = adB.node('.main-container'),
+        rangeInput = adB.node('input[type=range]'),
+        mainImage = adB.node('.image'),
+        descriptionContainer = adB.node('.description'),
+        fadeAria = adB.node('.fade-aria');
+
+
+    var MAIN_IMAGE_H = 221,
+        MAIN_IMAGE_MAX_I = 59,
+        DESCRIPTION_H = 108,
+        DESCRIPTION_STAT = 0;
+
+
+    adB.imageOnload(images, function (data) {
+
+        adB.addClass(mainContainer, 'show');
+
+        rangeInput.addEventListener('mousemove', function (event) {
+
+            mainImage.style.backgroundPositionY = -Number(event.target.value) * MAIN_IMAGE_H + 'px';
+
+            if (Number(event.target.value) < MAIN_IMAGE_MAX_I / 4) {
+                if (DESCRIPTION_STAT == 0) return;
+                adB.fadeDescription(fadeAria);
+
+                descriptionContainer.style.backgroundPositionY = 0;
+                DESCRIPTION_STAT = 0;
+            } else if (Number(event.target.value) < MAIN_IMAGE_MAX_I / 2.25) {
+                if (DESCRIPTION_STAT == 1) return;
+                adB.fadeDescription(fadeAria);
+
+                descriptionContainer.style.backgroundPositionY = -1 * DESCRIPTION_H + 'px';
+                DESCRIPTION_STAT = 1;
+            } else if (Number(event.target.value) < MAIN_IMAGE_MAX_I / 1.25) {
+                if (DESCRIPTION_STAT == 2) return;
+                adB.fadeDescription(fadeAria);
+
+                descriptionContainer.style.backgroundPositionY = -2 * DESCRIPTION_H + 'px';
+                DESCRIPTION_STAT = 2;
+            } else if (Number(event.target.value) < MAIN_IMAGE_MAX_I) {
+                if (DESCRIPTION_STAT == 3) return;
+                adB.fadeDescription(fadeAria);
+
+                descriptionContainer.style.backgroundPositionY = -3 * DESCRIPTION_H + 'px';
+                DESCRIPTION_STAT = 3;
+            }
+
+        }, false);
+
     });
 });
