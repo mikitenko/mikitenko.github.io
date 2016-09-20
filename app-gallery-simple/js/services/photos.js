3724d0e3b3400d222f9feb3dab52d5fe40818a02
Mikitenko <roman.mikiteko@maxymiser.com>
2016-09-20 16:25:45 +0300

    remove

--- app-gallery-simple/js/services/photos.js
+++
@@ -1,9 +0,0 @@
-app.factory('photos', ['$http', function($http) {
-    return $http.get('./photos.json')
-        .success(function(data) {
-            return data;
-        })
-        .error(function(data) {
-            return data;
-        });
-}]);
